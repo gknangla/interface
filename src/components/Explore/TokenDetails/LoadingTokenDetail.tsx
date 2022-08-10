d5c1ebf8386714d3fb4ee0633f45bffc42912eba
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-08-10 11:54:24 -0400

    fix

--- src/components/Explore/TokenDetails/LoadingTokenDetail.tsx
+++ src/components/Explore/TokenDetails/LoadingTokenDetail.tsx
@@ -61,7 +61,7 @@ export default function LoadingTokenDetail() {
   return (
     <TokenDetail
       breadcrumb={<Space heightSize={20} />}
-      tokenName={
+      tokenInfo={
         <>
           <IconLoadingBubble />
           <TitleLoadingBubble />
